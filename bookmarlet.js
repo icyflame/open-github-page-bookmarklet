ddafd1b149b10a88ea072d05ba3701ff94444234
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-26 12:23:58 +0530

    Initial commit. Add bookmarklet code.
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

---
+++ bookmarlet.js
@@ -0,0 +1,11 @@
+var https = document.URL;
+var matches = https.split('https://github.com/');
+if (matches.length <= 1) {
+	document.location = "https://github.com";
+} else {
+	matches = matches[1];
+	var username = matches.split('/')[0];
+	var repo = matches.split('/')[1];
+	var newURL = "https://" + username + ".github.io/" + repo;
+	document.location = newURL;
+}
